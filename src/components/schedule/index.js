2078bc9ce6b72bbe7aa37fc7b6569fee6aa8e403
Brandon Charles Spellman <brandoncharlesspellman@brandons-air.home>
2020-01-01 19:41:09 -0800

    updates

--- src/components/schedule/index.js
+++ src/components/schedule/index.js
@@ -18,6 +18,12 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
+
+import CardMedia from '@material-ui/core/CardMedia';
+let selection = []
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -52,6 +58,9 @@ function a11yProps(index) {
 const useStylesExpand = makeStyles(theme => ({
   root: {
     width: '100%',
+    display:'flex',
+    justifyContent:'center',
+    flexDirection:'column'
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
@@ -79,12 +88,59 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export function ControlledExpansionPanels() {
+export class Example extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.state = {
+      selectedDay: null,
+      string:''
+    };
+  }
+
+  handleDayClick(day, { selected }) {
+    this.setState({
+      selectedDay: selected ? undefined : day,
+    });
+    let days = selection.amountOfDays == undefined ? 1: selection.amountOfDays + 1;
+
+    let string = "You have chosen to run a promo on Hanna's: " + `${selection.platform} ` + ` ${day} ` + "for " + `${days}` + `${ days > 1 ? ' consecutive days ' : ' day ' }` + "for the amount of " + `$${selection.price} USD` + ' giving you ' + selection.hours + ' hours of promotion';
+    string = string.toString();
+    //alert(string)
+    this.setState({string})
+ 
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={{display:'flex',flexDirection:'column'}}>
+          <DayPicker
+            selectedDays={this.state.selectedDay}
+            onDayClick={this.handleDayClick}
+          />
+            <p>
+            {this.state.selectedDay
+            ? this.state.string
+            : 'Please select a day 👻'}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export function ControlledExpansionPanels(props) {
   const classes = useStylesExpand();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
+    
+    selection.platform = props.platform;
+    selection.price = props.price;
+    selection.hours = props.hours;
+    
   };
 
   return (
@@ -95,64 +151,29 @@ export function ControlledExpansionPanels() {
           aria-controls="panel1bh-content"
           id="panel1bh-header"
         >
-          <Typography className={classes.heading}>6 hours</Typography>
-          <Typography className={classes.secondaryHeading}>$5.00 USD</Typography>
+          <Typography className={classes.heading}>{props.hours}</Typography>
+          <Typography className={classes.secondaryHeading}>${props.price} USD</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>
-            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-            maximus est, id dignissim quam.
-          </Typography>
+          <div style={{display:'flex',justifyContent:'center',flexDirection:'column', alignItems:'center', width:'100%'}}>
+            <div>
+              <Example />
+            </div>
+          </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
-      <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2bh-content"
-          id="panel2bh-header"
-        >
-          <Typography className={classes.heading}>12 hours</Typography>
-          <Typography className={classes.secondaryHeading}>
-          $10.00 USD
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus, varius pulvinar
-            diam eros in elit. Pellentesque convallis laoreet laoreet.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography className={classes.heading}>24 hours</Typography>
-          <Typography className={classes.secondaryHeading}>
-            $20.00 USD
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-            vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-
     </div>
   );
 }
 
 
-export function SimpleTabs() {
+export function SimpleTabs(props) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-
+  
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    selection.amountOfDays = newValue;
   };
 
   return (
@@ -165,32 +186,97 @@ export function SimpleTabs() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <ControlledExpansionPanels/>
+      <CardMedia
+          style={{objectFit:'contain'}}
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={`${props.image}`}
+          title="Contemplative Reptile"
+        />
+        <div style={{margin:10}}>
+          <ControlledExpansionPanels hours={'6'} price={'5.00'} platform={props.platform} style={{margin:10}}/>
+        </div>
+        <div style={{margin:10}}>
+          <ControlledExpansionPanels hours={'12'} price={'10.00'} platform={props.platform} style={{margin:10}}/>
+        </div>
+        <div style={{margin:10}}>
+          <ControlledExpansionPanels hours={'24'} price={'20.00'} platform={props.platform} style={{margin:10}}/>
+        </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
-      <ControlledExpansionPanels/>
+      <CardMedia
+        style={{objectFit:'contain'}}
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={`${props.image}`}
+          title="Contemplative Reptile"
+        />
+      <div style={{margin:10}}>
+        <ControlledExpansionPanels hours={'6'} price={'5.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
+      <div style={{margin:10}}>
+        <ControlledExpansionPanels hours={'12'} price={'10.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
+      <div style={{margin:10}}> 
+        <ControlledExpansionPanels hours={'24'} price={'20.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
       </TabPanel>
       <TabPanel value={value} index={2}>
-      <ControlledExpansionPanels/>
-      </TabPanel>
+      <CardMedia
+                style={{objectFit:'contain'}}
+
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={`${props.image}`}
+          title="Contemplative Reptile"
+        />
+      <div style={{margin:10}}>
+        <ControlledExpansionPanels hours={'6'} price={'5.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
+      <div style={{margin:10}}>
+        <ControlledExpansionPanels hours={'12'} price={'10.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
+      <div style={{margin:10}}>
+        <ControlledExpansionPanels hours={'24'} price={'20.00'} platform={props.platform} style={{margin:10}}/>
+      </div>
+     </TabPanel>
     </div>
   );
 }
 
-export function Schedule() {
+export function Schedule(props) {
+  const [chosen, setChosen] = React.useState('');
   const classes = useStyles();
   return (
     <div style={{display:'flex',justifyContent:'center'}}>
       <div style={{width:'100%',maxWidth:500}}>
         <Card className={classes.card}>
-        <SimpleTabs/>
-        <Button variant="contained" color="primary">CHOOSE</Button>
-        </Card>
-        <div><p>or</p></div>
-        <Card className={classes.card}>
-        <SimpleTabs/>
-        <Button variant="contained" color="primary">CHOOSE</Button>
+          <SimpleTabs image={'https://ed-projects.com/wp-content/uploads//instagram.jpg'} platform={'instagram'}/>
+          <div style={{display:'flex',width:'100%',justifyContent:'center',padding:'15px 0px'}}>
+            <div style={{display:'flex',flexDirection:'column', maxWidth:400, width:'100%'}}>
+            <div style={{maxWidth:800, width:'100%'}}>
+              {chosen}
+            </div>
+            <Button onClick={()=>{
+          
 
+            }} variant="contained" color="primary">CHOOSE</Button>
+            <div>
+              
+            </div>
+            </div>
+          </div>
+        </Card>
+        <div style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}><p>or</p></div>
+        <Card className={classes.card}>
+          <SimpleTabs image={'https://upload.wikimedia.org/wikipedia/commons/0/0a/OnlyFans_logo_3.jpg'} platform={'onlyfans'}/>
+          <div style={{display:'flex',width:'100%',justifyContent:'center'}}>
+            <Button variant="contained" color="primary">CHOOSE</Button>
+            {chosen}
+          </div>
         </Card>
       </div>
     </div>
